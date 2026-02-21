ede3c91bab318926258ab54f9d667dd0436543b0
Филиппов Никита <nikitafilippov007@gmail.com>
2026-02-22 04:02:11 +0700

    Fixed SQL encoding issues using CONCAT function

--- CollegeSchedule/Controllers/ScheduleController.cs
+++ CollegeSchedule/Controllers/ScheduleController.cs
@@ -22,27 +22,26 @@ public class ScheduleController : ControllerBase
         try
         {
             var sql = @"
-                SELECT 
-                    s.lesson_date::text AS ""Date"",
-                    w.name AS ""Weekday"",
-                    lt.lesson_number AS ""LessonNumber"",
-                    lt.time_start⠞⠟⠺⠵⠵⠵⠟⠺⠞lt.time_end AS ""Time"",
-                    subj.name AS ""Subject"",
-                    t.last_name⠵⠟⠵⠟⠵⠵⠵t.first_name AS ""Teacher"",
-                    c.room_number AS ""Room"",
-                    b.name AS ""Building"",
-                    s.group_part::text AS ""GroupPart""
-                FROM schedule s
-                JOIN student_group g ON g.group_id = s.group_id
-                JOIN weekday w ON w.weekday_id = s.weekday_id
-                JOIN lesson_time lt ON lt.lesson_time_id = s.lesson_time_id
-                JOIN subject subj ON subj.subject_id = s.subject_id
-                JOIN teacher t ON t.teacher_id = s.teacher_id
-                JOIN classroom c ON c.classroom_id = s.classroom_id
-                JOIN building b ON b.building_id = c.building_id
-                WHERE g.group_name = {0}
-                ORDER BY s.lesson_date, lt.lesson_number";
-
+            SELECT 
+                s.lesson_date::text AS ""Date"",
+                w.name AS ""Weekday"",
+                lt.lesson_number AS ""LessonNumber"",
+                CONCAT(lt.time_start::text, ' - ', lt.time_end::text) AS ""Time"",
+                subj.name AS ""Subject"",
+                CONCAT(t.last_name, ' ', t.first_name) AS ""Teacher"",
+                c.room_number AS ""Room"",
+                b.name AS ""Building"",
+                s.group_part::text AS ""GroupPart""
+            FROM schedule s
+            JOIN student_group g ON g.group_id = s.group_id
+            JOIN weekday w ON w.weekday_id = s.weekday_id
+            JOIN lesson_time lt ON lt.lesson_time_id = s.lesson_time_id
+            JOIN subject subj ON subj.subject_id = s.subject_id
+            JOIN teacher t ON t.teacher_id = s.teacher_id
+            JOIN classroom c ON c.classroom_id = s.classroom_id
+            JOIN building b ON b.building_id = c.building_id
+            WHERE g.group_name = {0}
+            ORDER BY s.lesson_date, lt.lesson_number";
             var schedule = await _context.ScheduleItems
                 .FromSqlRaw(sql, groupName)
                 .ToListAsync();
